f0bac1f9c31f519fc1a8684721ffed69c88f61e5
Rcmcpe <rcmcpe-cn@outlook.com>
2022-06-18 20:28:47 +0800

    Fix handle leak and various bugs

--- Quiet.cpp
+++ Quiet.cpp
@@ -2,8 +2,8 @@
 #include <Windows.h>
 using namespace std;
 
-LPCWSTR info_help = LR"(Quiet v1.0 RcINS 2021
-Creates a process without window (CREATE_NO_WINDOW).
+LPCWSTR help_info = LR"(Quiet v1.1 RcINS 2022
+Creates a process without windows (CreateProcessW, CREATE_NO_WINDOW).
 
 Usage: quiet.exe cmdLine
 
@@ -14,14 +14,25 @@ Remarks:
 1. Quiet creates no window unless it's displaying help.
 2. Created process inherits working directory from Quiet.)";
 
-int APIENTRY wWinMain(HINSTANCE, HINSTANCE, LPWSTR lpCmdLine, int)
+int APIENTRY wWinMain(
+	_In_ HINSTANCE,
+	_In_opt_ HINSTANCE,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int)
 {
 	if (wcslen(lpCmdLine) == 0 || wcscmp(lpCmdLine, L"/?") == 0)
 	{
-		MessageBoxW(nullptr, info_help, L"Help: Quiet", MB_OK);
+		MessageBoxW(nullptr, help_info, L"Help: Quiet", MB_OK);
+		return 1;
 	}
-	STARTUPINFOW si{};
-	PROCESS_INFORMATION pi;
-	CreateProcess(nullptr, lpCmdLine, nullptr, nullptr, false,
-	              CREATE_NO_WINDOW, nullptr, nullptr, &si, &pi);
+	STARTUPINFOW startup_info{};
+	PROCESS_INFORMATION process_info{};
+	if (!CreateProcess(nullptr, lpCmdLine, nullptr, nullptr, false,
+	                   CREATE_NO_WINDOW, nullptr, nullptr, &startup_info, &process_info))
+	{
+		return -1;
+	}
+	CloseHandle(process_info.hProcess);
+	CloseHandle(process_info.hThread);
+	return 0;
 }
